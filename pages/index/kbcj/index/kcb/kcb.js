25b817c7cff4b190fa58f19f665eebb1e16f46bd
Ender <1286554811@qq.com>
2019-11-04 21:58:15 +0800

    为课程表添加学习周选项

--- pages/index/kbcj/index/kcb/kcb.js
+++ pages/index/kbcj/index/kcb/kcb.js
@@ -9,159 +9,167 @@ var currentDay = date.getDate();
 var currentWeek = weeks[date.getUTCDay()];
 var week = [];
 for (var i = 0; i < currentWeek; i++) {
-    week[i] = currentDay - currentWeek + i + 1;
-    if (week[i] <= 0)
-        week[i] = week[i] + month[currentMonth - 2];
+  week[i] = currentDay - currentWeek + i + 1;
+  if (week[i] <= 0)
+    week[i] = week[i] + month[currentMonth - 2];
 }
 for (var i = currentWeek, j = 1; i < 7; i++, j++) {
-    week[i] = currentDay + j;
-    if (week[i] > month[currentMonth - 1])
-        week[i] = week[i] - month[currentMonth - 1];
+  week[i] = currentDay + j;
+  if (week[i] > month[currentMonth - 1])
+    week[i] = week[i] - month[currentMonth - 1];
 }
 
 const app = getApp();
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        stateH: hei,
-        year: currentYear,
-        month: currentMonth,
-        day: currentDay,
-        classArray: ["1\\2", "3\\4", "5\\6", "7\\8", "9\\10"],
-        dayArray: [{
-            index: 1,
-            day: week[0]
-        }, {
-            index: 2,
-            day: week[1]
-        }, {
-            index: 3,
-            day: week[2]
-        }, {
-            index: 4,
-            day: week[3]
-        }, {
-            index: 5,
-            day: week[4]
-        }, {
-            index: 6,
-            day: week[5]
-        }, {
-            index: 7,
-            day: week[6]
-        }],
-        listData: null,
-        allData: null,
-        cout: 0
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    stateH: hei,
+    year: currentYear,
+    month: currentMonth,
+    day: currentDay,
+    schoolWeek: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    weekIndex: 0,
+    classArray: ["1\\2", "3\\4", "5\\6", "7\\8", "9\\10"],
+    dayArray: [{
+      index: 1,
+      day: week[0]
+    }, {
+      index: 2,
+      day: week[1]
+    }, {
+      index: 3,
+      day: week[2]
+    }, {
+      index: 4,
+      day: week[3]
+    }, {
+      index: 5,
+      day: week[4]
+    }, {
+      index: 6,
+      day: week[5]
+    }, {
+      index: 7,
+      day: week[6]
+    }],
+    listData: null,
+    allData: null,
+    cout: 0
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function(options) {
-        var that = this;
-        wx.showLoading();
-        app.http.JwKb().then((res) => {
-            wx.hideLoading();
-            var jsons = JSON.parse(res.data['kb'])
-            var listData = that.dealData(jsons);
-            that.setData({
-                listData: listData,
-                allData: jsons
-            })
-        }, (error) => {
-            wx.hideLoading();
-            wx.showModal({
-                title: '错误',
-                content: '网络错误，请重新登录',
-                success() {
-                    wx.navigateTo({
-                        url: '../../kbcj',
-                    })
-                }
-            })
-        })
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
-
-    },
-    dealData: function(data) {
-        var newData = Array();
-        var temp = 0;
-        var a = 1;
-        var b = 1;
-        for (var i = 0; i <= 4; ++i) {
-            var nowData = Array();
-            for (var j = 0; j <= 6; ++j) {
-                var str = a.toString() + '-' + b.toString() + '-1';
-                nowData.push(data[temp++][0][str]);
-                b++;
-            }
-            a++;
-            b = 1;
-            newData.push(nowData);
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    var that = this;
+    wx.showLoading();
+    app.http.JwKb().then((res) => {
+      wx.hideLoading();
+      var jsons = JSON.parse(res.data['kb'])
+      var listData = that.dealData(jsons);
+      that.setData({
+        listData: listData,
+        allData: jsons
+      })
+    }, (error) => {
+      wx.hideLoading();
+      wx.showModal({
+        title: '错误',
+        content: '网络错误，请重新登录',
+        success() {
+          wx.navigateTo({
+            url: '../../kbcj',
+          })
         }
-        console.log(newData)
-        return newData
-    },
-    showDetail: function(e) {
-        var nowuse = Number(e.currentTarget.id);
-        var data = this.data.allData;
-        var str = (Math.floor(nowuse / 7) + 1).toString() + '-' + (nowuse % 7 + 1).toString();
-        console.log(str)
-        wx.showModal({
-            title: '详细信息',
-            content: '课程名：' + data[nowuse][0][str + '-1'] + "\r\n" + "详细信息：" + data[nowuse][1][str + '-2'] + "\r\n" + "其他：" + (data[nowuse][1][str + '-3'] == undefined ? "" : data[nowuse][1][str + '-3']),
-            showCancel: false
-        })
+      })
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+  pickerChange: function(e) {
+    console.log(e.detail.value);
+    this.setData({
+      weekIndex:e.detail.value,
+    })
+  },
+  dealData: function(data) {
+    var newData = Array();
+    var temp = 0;
+    var a = 1;
+    var b = 1;
+    for (var i = 0; i <= 4; ++i) {
+      var nowData = Array();
+      for (var j = 0; j <= 6; ++j) {
+        var str = a.toString() + '-' + b.toString() + '-1';
+        nowData.push(data[temp++][0][str]);
+        b++;
+      }
+      a++;
+      b = 1;
+      newData.push(nowData);
     }
+    console.log(newData)
+    return newData
+  },
+  showDetail: function(e) {
+    var nowuse = Number(e.currentTarget.id);
+    var data = this.data.allData;
+    var str = (Math.floor(nowuse / 7) + 1).toString() + '-' + (nowuse % 7 + 1).toString();
+    console.log(str)
+    wx.showModal({
+      title: '详细信息',
+      content: '课程名：' + data[nowuse][0][str + '-1'] + "\r\n" + "详细信息：" + data[nowuse][1][str + '-2'] + "\r\n" + "其他：" + (data[nowuse][1][str + '-3'] == undefined ? "" : data[nowuse][1][str + '-3']),
+      showCancel: false
+    })
+  }
 })
